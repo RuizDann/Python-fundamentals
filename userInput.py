c867c7c48453432ca424f6a4f42a1b91a70435a7
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-12 21:14:19 -0500

    working with prints, inputs, and variables

---
+++ userInput.py
@@ -0,0 +1,16 @@
+# ITP Week 1 Day 1 Exercise
+
+# declare a VARIABLE named my_favorite_band and ASSIGN it a STRING value of your choice.
+
+myFavBand = "Blink182"
+
+# PRINT a CONCATENATION of the variable and a string literal: " is my favorite band!"
+print(myFavBand + " is my favorite band!")
+
+# prompt an user INPUT to ask "What is your favorite color?" and assign it to a variable called "user_favorite_color"
+
+userFavColor = input("What is your favorite color?")
+
+# PRINT a string concatenation with user_favorite_color and "is a nice color!"
+
+print(userFavColor + " is a nice color!")
