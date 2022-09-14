e637a0b77f1314a2352629a07832672e86e9775c
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-13 23:00:00 -0500

    Added code to satisfy directions

--- simpleMath.py
+++ simpleMath.py
@@ -3,3 +3,12 @@
 # c. assign a variable 'taxAmount' to the product of subtotal and taxPer
 # d. assign a variable 'total' to the sum of subtotal and taxAmount
 
+subtotal = 17.75
+taxPer = .18
+taxAmount = subtotal * taxPer
+total = subtotal + taxAmount
+
+print(f'''Subtotal: {subtotal}
+Tax percentage: {taxPer}
+Tax amount: {round(taxAmount, 2)}
+Total: {round(total, 2)}''')
