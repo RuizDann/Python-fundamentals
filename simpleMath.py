c7abd7faf5f934062860f7c99acb20c0d1ee8a7f
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-13 22:58:25 -0500

    added directions

---
+++ simpleMath.py
@@ -0,0 +1,5 @@
+# a. assign a variable 'subtotal' to 17.75
+# b. assign a variable 'taxPer' to .18
+# c. assign a variable 'taxAmount' to the product of subtotal and taxPer
+# d. assign a variable 'total' to the sum of subtotal and taxAmount
+
