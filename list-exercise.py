c7a7bbf8c7cb06cb8caff144f7751a27acd2ba1e
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-14 21:26:46 -0500

    fixed list

--- list-exercise.py
+++ list-exercise.py
@@ -1,13 +1,17 @@
 # Use the following list to complete the following exercises:
-us_states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
-"Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii",
-"Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
-"Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
-"Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
-"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
-"South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
-"Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+us_states = ["Alabama", "Alaska", "Arizona", "Arkansas", 
+"California", "Colorado", "Connecticut", "Delaware", 
+"Florida", "Georgia", "Hawaii", "Idaho", 
+"Illinois", "Indiana", "Iowa", "Kansas", 
+"Kentucky", "Louisiana", "Maine", "Maryland", 
+"Massachusetts", "Michigan", "Minnesota", "Mississippi", 
+"Missouri", "Montana", "Nebraska", "Nevada", 
+"New Hampshire", "New Jersey", "New Mexico", "New York", 
+"North Carolina", "North Dakota", "Ohio", "Oklahoma", 
+"Oregon", "Pennsylvania", "Rhode Island", "South Carolina", 
+"South Dakota", "Tennessee", "Texas", "Utah", 
+"Vermont", "Virginia", "Washington", "West Virginia", 
+"Wisconsin", "Wyoming"]
 
 # print the LENgth of us_states
 print(len(us_states)) #50
