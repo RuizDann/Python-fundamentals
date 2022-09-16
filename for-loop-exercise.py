b71619e3d79911e50103ddd56691c29790ac1078
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-16 12:33:53 -0500

    formatted list

--- for-loop-exercise.py
+++ for-loop-exercise.py
@@ -1,6 +1,10 @@
 # EASY
 
-lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+lowercase = ['a', 'b', 'c', 'd', 'e',
+             'f', 'g', 'h', 'i', 'j',
+             'k', 'l', 'm', 'n', 'o',
+             'p', 'q', 'r', 's', 't',
+             'u', 'v', 'w', 'x', 'y', 'z']
 
 # 1. loop through the lowercase and print each element
 
